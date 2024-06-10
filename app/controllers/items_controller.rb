33fe0e7a620881539ffb5ffe3a22caf3d5e36892
ooooeaa <sodecoo0621@gmail.com>
2024-06-10 18:22:43 -0400

    ActiveStorageインストール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @items = Item.all
+  end
+
+  def create
+    Item.create(Item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,:price, :image).merge(user_id: current_user.id)
+  end
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+  belongs_to :order
+  belongs_to :user
+  has_one_attached :image
+  
+  validates :name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: true
+  validates :price, presence: true
+
+  
+
+end
