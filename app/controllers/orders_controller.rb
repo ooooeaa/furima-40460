d57b2f795c070e22e133a5c010b5211d498d9e94
ooooeaa <sodecoo0621@gmail.com>
2024-06-30 12:58:40 -0400

    配送先情報入力機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,22 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :new, :create, :show]
 
-  def new
+  def index
     @order_address = OrderAddress.new
   end
+  
+  def new
+    @order_address = OrderAddress.new(item_id: params[:item_id])
+  end
+  
 
   def create
-    @order = Order.new(user_id: order_params[:user_id], item_id: order_params[:item_id])
-    if @order.save
-      @address = Address.new(address_params.merge(order_id: @order.id))
-      if @address.save
-        redirect_to root_path, notice: '注文が完了しました。'
-      else
-        @order.destroy
-        render :new, status: :unprocessable_entity
-      end
+    @order_address = OrderAddress.new(order_params.merge(user_id: current_user.id, item_id: params[:item_id]))
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
@@ -27,10 +27,8 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:user_id, :item_id)
+   params.require(:order_address).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
   end
 
-  def address_params
-    params.require(:order_address).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number)
-  end
 end
+
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,25 +1,32 @@
 class OrderAddress
   include ActiveModel::Model
+  #DonationAddressクラス内でattr_accessorを使用します。
+#donationsテーブルとaddressesテーブルに保存したいカラム名を、すべて指定してください。
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
 
+  validates :user_id, presence: true
+  validates :item_id, presence: true
   validates :city, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
   validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
+  
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(
-      order_id: order_id, 
-      postal_code: postal_code, 
-      prefecture_id: prefecture_id, 
-      city: city, 
-      address: address, 
-      building: building, 
-      phone_number: phone_number
-    )
-    true
-  end
+   # Orderテーブルにデータを保存
+   order = Order.create(user_id: user_id, item_id: item_id)
+    
+   # Addressテーブルにデータを保存
+   Address.create(
+     order_id: order.id,
+     postal_code: postal_code,
+     prefecture_id: prefecture_id,
+     city: city,
+     address: address,
+     building: building,
+     phone_number: phone_number
+   )
+ end
 end
 
