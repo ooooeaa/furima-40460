963baea8900f3c974bd84a698d870a290b459a24
ooooeaa <sodecoo0621@gmail.com>
2024-07-08 21:40:42 -0400

    挙動確認後修正2

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,7 +12,7 @@ class OrderAddress
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :token
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   end
 
   def save
